f2fc7be23043c08371004e9534a02cabbe3bd493
mollaawoke4-spec <mollaawoke4@gmail.com>
2025-12-04 20:47:32 +0300

    Direct Data Addressing.asm

--- 11.asm
+++ 11.asm
@@ -1,11 +1,19 @@
-INFO    STRUC
-NAMES   DB 32 DUP(?)
-STREET  DB 32 DUP(?)
-CITY    DB 16 DUP(?)
-STATE   DB 2 DUP(?)
-ZIP     DB 5 DUP(?)
-INFO    ENDS
+.MODEL SMALL              ; Choose small memory model
+.STACK 100h              ; Allocate 256 bytes for stack
 
-NAME1   INFO <'Bob Smith', '123 Main Street', 'Wanda', 'OH', '44444'>
-NAME2   INFO <'Steve Doe', '222 Moose Lane', 'Miller', 'PA', '18100'>
-NAME3   INFO <'Jim Dover', '303 Main Street', 'Orender', 'CA', '90000'>
+.DATA                    ; Start of data segment
+    DATA1 DB 10h         ; Initialize DATA1 with 10h
+    DATA2 DB 0           ; Initialize DATA2 with 00h
+    DATA3 DW 0           ; Initialize DATA3 with 0000h
+    DATA4 DW 0AAAAh      ; Initialize DATA4 with AAAAh
+
+.CODE                    ; Start of code segment
+.STARTUP                 ; Program entry point
+
+    MOV AL, DATA1        ; Load DATA1 into AL
+    MOV AH, DATA2        ; Load DATA2 into AH
+    MOV DATA3, AX        ; Store AX into DATA3
+    MOV BX, DATA4        ; Load DATA4 into BX
+
+.EXIT                    ; Exit to DOS
+END                      ; End of program
